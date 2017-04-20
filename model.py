fd5a7090f965786e8a693f0093f0a96b62a0b527
Esteve Soria <esofabian@gmail.com>
2017-04-20 18:27:23 +0200

    More complex model, Generator added

--- model.py
+++ model.py
@@ -1,10 +1,13 @@
+import os
 import csv
 import cv2
 import numpy as np
+import sklearn
 
 from keras.models import Sequential
-from keras.layers import Flatten, Dense, Conv2D, Dropout, MaxPooling2D, Activation
+from keras.layers import Flatten, Dense, Conv2D, Dropout, MaxPooling2D, Activation, Lambda
 
+'''
 lines = []
 with open('./data/driving_log.csv') as csvfile:
     reader = csv.reader(csvfile)
@@ -25,9 +28,49 @@ y_train = np.array(measurements)
 
 input_shape = X_train[0].shape
 print(input_shape)
+'''
+samples = []
+with open('./data/driving_log.csv') as csvfile:
+    reader = csv.reader(csvfile)
+    for line in reader:
+        samples.append(line)
+
+from sklearn.model_selection import train_test_split
+train_samples, validation_samples = train_test_split(samples, test_size=0.2)
+
+def generator(samples, batch_size=32):
+    num_samples = len(samples)
+    while 1: # Loop forever so the generator never terminates
+        for offset in range(0, num_samples, batch_size):
+            batch_samples = samples[offset:offset+batch_size]
+
+            images = []
+            angles = []
+            for batch_sample in batch_samples:
+                name = './data/IMG/'+batch_sample[0].split('/')[-1]
+                center_image = cv2.imread(name)
+                center_angle = float(batch_sample[3])
+                images.append(center_image)
+                angles.append(center_angle)
+
+            # trim image to only see section with road
+            X_train = np.array(images)
+            y_train = np.array(angles)
+            yield sklearn.utils.shuffle(X_train, y_train)
+
+BATCH_SIZE = 32
+
+train_generator = generator(train_samples, batch_size=BATCH_SIZE)
+validation_generator = generator(validation_samples, batch_size=BATCH_SIZE)
+
+ch, row, col = 3, 160, 320
+
+def normalize(x):
+    return x/127.5 - 1
 
 model = Sequential()
-model.add(Conv2D(32, (3, 3), input_shape=input_shape))
+model.add(Lambda(normalize, input_shape=(row, col, ch)))
+model.add(Conv2D(32, (3, 3)))
 model.add(MaxPooling2D((2, 2)))
 model.add(Dropout(0.5))
 model.add(Activation('relu'))
@@ -38,6 +81,7 @@ model.add(Dense(1))
 model.add(Activation('softmax'))
 
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, batch_size=16, validation_split=0.2, shuffle=True, epochs=7)
+model.fit_generator(train_generator, steps_per_epoch=(len(train_samples)/BATCH_SIZE), 
+                    validation_data=validation_generator, validation_steps=(len(validation_samples)/BATCH_SIZE), epochs=3)
 
 model.save('model.h5')

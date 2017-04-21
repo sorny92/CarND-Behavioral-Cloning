e40d7ece30e0473f3bd84aa8c92bae5b48c94a85
Esteve Soria <esofabian@gmail.com>
2017-04-21 20:11:46 +0200

    Architecture improved. Let's add more data

--- model.py
+++ model.py
@@ -5,7 +5,7 @@ import numpy as np
 import sklearn
 
 from keras.models import Sequential
-from keras.layers import Flatten, Dense, Conv2D, Dropout, MaxPooling2D, Activation, Lambda
+from keras.layers import Flatten, Dense, Conv2D, Dropout, MaxPooling2D, Activation, Lambda, Cropping2D
 
 '''
 lines = []
@@ -13,7 +13,6 @@ with open('./data/driving_log.csv') as csvfile:
     reader = csv.reader(csvfile)
     for line in reader:
         lines.append(line)
-
 images = []
 measurements = []
 for line in lines:
@@ -22,15 +21,13 @@ for line in lines:
     images.append(image)
     measurement = float(line[3])
     measurements.append(measurement)
-
 X_train = np.array(images)
 y_train = np.array(measurements)
-
 input_shape = X_train[0].shape
 print(input_shape)
 '''
 samples = []
-with open('./data/driving_log.csv') as csvfile:
+with open('./data_v2/driving_log.csv') as csvfile:
     reader = csv.reader(csvfile)
     for line in reader:
         samples.append(line)
@@ -47,18 +44,23 @@ def generator(samples, batch_size=32):
             images = []
             angles = []
             for batch_sample in batch_samples:
-                name = './data/IMG/'+batch_sample[0].split('/')[-1]
+                name = './data_v2/IMG/'+batch_sample[0].split('/')[-1]
                 center_image = cv2.imread(name)
                 center_angle = float(batch_sample[3])
+                image_flipped = np.fliplr(center_image)
+                image_flipped_angle = -center_angle
+                images.append(image_flipped)
+                angles.append(image_flipped_angle)
                 images.append(center_image)
                 angles.append(center_angle)
 
-            # trim image to only see section with road
+            # trim image t o only see section with road
             X_train = np.array(images)
             y_train = np.array(angles)
             yield sklearn.utils.shuffle(X_train, y_train)
 
-BATCH_SIZE = 32
+BATCH_SIZE = 4
+EPOCHS = 2
 
 train_generator = generator(train_samples, batch_size=BATCH_SIZE)
 validation_generator = generator(validation_samples, batch_size=BATCH_SIZE)
@@ -69,19 +71,35 @@ def normalize(x):
     return x/127.5 - 1
 
 model = Sequential()
-model.add(Lambda(normalize, input_shape=(row, col, ch)))
-model.add(Conv2D(32, (3, 3)))
+model.add(Cropping2D(cropping=((50,20), (0,0)), input_shape=(row, col, ch)))
+model.add(Lambda(normalize))
+model.add(Conv2D(15, (2, 2)))
+model.add(MaxPooling2D((2, 2)))
+model.add(Dropout(0.3))
+model.add(Conv2D(36, (3, 3)))
+model.add(MaxPooling2D((2, 2)))
+model.add(Dropout(0.5))
+model.add(Flatten())
+model.add(Dense(10))
+model.add(Dense(1))
+'''model.add(Conv2D(10, (3, 3)))
+model.add(MaxPooling2D((2, 2)))
+model.add(Dropout(0.5))
+model.add(Activation('relu'))
+model.add(Conv2D(12, (4, 4)))
 model.add(MaxPooling2D((2, 2)))
 model.add(Dropout(0.5))
 model.add(Activation('relu'))
 model.add(Flatten())
-model.add(Dense(128))
+model.add(Dense(60))
+model.add(Activation('relu'))aw
+model.add(Dense(90))
 model.add(Activation('relu'))
 model.add(Dense(1))
-model.add(Activation('softmax'))
+model.add(Activation('softmax'))'''
 
 model.compile(loss='mse', optimizer='adam')
 model.fit_generator(train_generator, steps_per_epoch=(len(train_samples)/BATCH_SIZE), 
-                    validation_data=validation_generator, validation_steps=(len(validation_samples)/BATCH_SIZE), epochs=3)
+                    validation_data=validation_generator, validation_steps=(len(validation_samples)/BATCH_SIZE), epochs=EPOCHS)
 
 model.save('model.h5')
